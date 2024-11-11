eeafed44064caf8cce78016f43ededc242b507c3
Josh Voyles <120749218+Josh-Voyles@users.noreply.github.com>
2024-11-10 20:08:52 -0500

    added ai backend, connected strings, refactored codebase with pylint

--- virin-xmp-toolkit/models/meta_edit.py
+++ virin-xmp-toolkit/models/meta_edit.py
@@ -1,30 +1,28 @@
+"""
+Module Name: file-rename
+Author: Josh Voyles
+Created: 28 Oct 24
+
+Description:
+
+This module retrieves and batch overwirtes metadata for photo/video files in selected directory.
+Will retreive metadata for first file in directory.
+
+Much work needs to be done to support additional meta fields and append to correct location.
+"""
+
 import os
-import exiftool
 from exiftool import ExifToolHelper
 
 
 class MetaTool:
+    """
+    Provides functionality to write and retrieve metadata using the ExifTool library.
+    The metadata can be added to and extracted from various files based on the file extension.
+    """
 
-    def write_metadata(self, path, selected_extension, metadata: dict) -> str:
-        """Triggers exiftool to write metadata"""
-        files = []
-        try:
-            files = [
-                os.path.join(path, file)
-                for file in os.listdir(path)
-                if file.lower().endswith(selected_extension)
-            ]
-        except FileNotFoundError:
-            return "Please choose a folder."
-        if files:
-            with ExifToolHelper() as et:
-                et.set_tags(files, tags=metadata, params=["-P", "-overwrite_original"])
-                et.terminate()
-            return "Metadata updated sucessfully!"
-        return f"No files found with extension {selected_extension}."
-
-    def retreive_metadata(self, path, selected_extension) -> dict:
-        data = {
+    def __init__(self) -> None:
+        self.meta_fields = {
             "Creator": "",
             "Writer": "",
             "Description": "",
@@ -36,36 +34,73 @@ class MetaTool:
             "State": "",
             "Copyright": "",
         }
+
+    def _load_files(self, path, selected_extension) -> list:
+        """
+        Load files from specified directory.
+
+        Parameters:
+            path (str): The directory path containing files to modify.
+            selected_extension (str): The file extension filter to select specific files.
+
+        Raises:
+            FileNotFoundError: If the specified directory path does not exist.
+        """
         try:
-            files = [
+            return [
                 os.path.join(path, file)
                 for file in os.listdir(path)
                 if file.lower().endswith(selected_extension)
             ]
         except FileNotFoundError:
-            return data
+            return []
 
+    def write_metadata(self, path, selected_extension, metadata: dict) -> str:
+        """
+
+        Writes metadata to files within the specified directory.
+
+        Parameters:
+            path (str): The directory path containing files to modify.
+            selected_extension (str): The file extension filter to select specific files.
+            metadata (dict): The metadata tags to be written to the selected files.
+
+        Returns:
+            str: A message indicating the success or failure of the metadata update operation.
+
+        Raises:
+            FileNotFoundError: If the specified directory path does not exist.
+        """
+        files = self._load_files(path, selected_extension)
+        if files:
+            with ExifToolHelper() as et:
+                et.set_tags(files, tags=metadata, params=["-P", "-overwrite_original"])
+                et.terminate()
+            return "Metadata updated sucessfully!"
+        return f"No files found with extension {selected_extension} \
+                in {"directory" if path else "(Unspecified directory)"}."
+
+    def retreive_metadata(self, path, selected_extension) -> dict:
+        """
+        Retrieves metadata from first file of specified directory.
+
+        Args:
+            path: The directory path where the files are located.
+            selected_extension: The file extension to filter the files.
+
+        Returns:
+            A dictionary containing metadata information.
+            Default empty values are returned for each key if nothing found.
+        """
+
+        files = self._load_files(path, selected_extension)
         # in the function, we will just display meta of first file since batch processing
         if files:
             with ExifToolHelper() as et:
-                tags = et.get_tags(
-                    files[0],
-                    tags=[
-                        "Creator",
-                        "Writer",
-                        "Description",
-                        "Title",
-                        "Keywords",
-                        "City",
-                        "Country",
-                        "Headline",
-                        "State",
-                        "Copyright",
-                    ],
-                )
+                tags = et.get_tags(files[0], tags=list(self.meta_fields.keys()))
                 for key, value in tags[0].items():
                     key = key.split(":")
-                    if len(key) > 1 and key[1] in data:
-                        data[key[1]] = value if data[key[1]] == "" else data[key[1]]
+                    if len(key) > 1 and key[1] in self.meta_fields:
+                        self.meta_fields[key[1]] = value
                 et.terminate()
-        return data
+        return self.meta_fields
--- virin-xmp-toolkit/views/main_window.py
+++ virin-xmp-toolkit/views/main_window.py
@@ -1,30 +1,52 @@
 """
-Description
-    
+Module Name: main_window
+Author: Josh Voyles
+Created: 28 Oct 24
+
+Description:
+
+This module tries the front end gui to backend for virin xmp toolkit.
+
 """
 
 from PyQt6.QtWidgets import QFileDialog, QMainWindow, QMessageBox
 from PyQt6.QtGui import QRegularExpressionValidator
-from PyQt6.QtCore import Q_RETURN_ARG, QRegularExpression
+from PyQt6.QtCore import QCoreApplication, QRegularExpression
 from models.file_rename import FileRenamer
 from models.meta_edit import MetaTool
+from models.ai_backend import VIRINAI
 from views.main_window_ui import Ui_MainWindow
+
 
 FILENAME_PAGE_INDEX = 0
 METADATA_PAGE_INDEX = 1
 AI_CAPTION_PAGE_INDEX = 2
+EMPTY_STRING = ""
+DEFAULT_CREATOR = "USAF Band Production"
+DEFAULT_KEYWORD = "USAFBand"
+PUBLIC_DOMAIN_COPYRIGHT = "Public Domain"
 
 
 class MainWindow(QMainWindow):
+    """
+    MainWindow class that manages the user interface for renaming files and editing metadata.
+    """
+
     def __init__(self):
+        """
+        Initializes the main window and sets up the user interface components,
+        validators, and signals for various buttons.
+        """
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.retranslateUi(self)
         self.current_window_index = 0
-        self.file_path = ""
+        self.file_path = EMPTY_STRING
+        # dependencies
         self.fr = FileRenamer()
         self.meta = MetaTool()
+        self.ai = VIRINAI()
 
         self.setWindowTitle("VIRIN XMP Toolkit")
 
@@ -33,7 +55,7 @@ class MainWindow(QMainWindow):
             r"(20|19)\d{2}(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])$"
         )
         shot_regex = QRegularExpression(r"\d$")
-        seq_regex = QRegularExpression(r"\d\d\d$")
+        seq_regex = QRegularExpression(r"\d{3}$")
         date_validator = QRegularExpressionValidator(date_regex)
         shot_validator = QRegularExpressionValidator(shot_regex)
         seq_validator = QRegularExpressionValidator(seq_regex)
@@ -41,6 +63,7 @@ class MainWindow(QMainWindow):
         self.ui.shotEdit.setValidator(shot_validator)
         self.ui.seqEdit.setValidator(seq_validator)
 
+        # connect buttons to functions
         self.ui.pushButton.clicked.connect(self.display_filename_page)
         self.ui.metaButton.clicked.connect(self.display_metadata_page)
         self.ui.aiButton.clicked.connect(self.display_ai_page)
@@ -52,16 +75,36 @@ class MainWindow(QMainWindow):
         self.ui.loadButton.clicked.connect(self.load_metadata)
         self.ui.clearButton.clicked.connect(self.clear_metadata_fields)
         self.ui.writeButton.clicked.connect(self.write_metadata_to_files)
+        self.ui.aiSubmitButton.clicked.connect(self.prompt_ai)
+        self.ui.aiResetButton.clicked.connect(self.clear_ai_fields)
+
+    def _display_empty_shot_seq_warning(self):
+        """Displays a warning message if shot or sequence numbers are not provided."""
+        message = "Please enter a shot and sequence number"
+        QMessageBox.warning(self, "Number Error", message)
+
+    def _display_empty_path_warning(self):
+        """Displays a warning message if no file path is selected."""
+        if not self.file_path:
+            message = "Please choose a file path"
+            QMessageBox.warning(self, "Path Error", message)
+
+    def _get_file_format(self):
+        """Retrieves the current file format based on the selected page."""
+        if self.current_window_index == FILENAME_PAGE_INDEX:
+            return self.ui.fileFormatComboBox.currentText()
+        return self.ui.metaFileFormatComboBox.currentText()
 
     # Functions listed in priority top down
     def open_folder_chooser(self):
+        """Opens a dialog to choose a folder and sets the selected folder path."""
         # TODO fix large dialog size
-        # TODO Confirm slash will never be added
         folder_chooser = QFileDialog(self)
-        path = folder_chooser.getExistingDirectory(None, "Select a Folder", "")
+        path = folder_chooser.getExistingDirectory(
+            None, "Select a Folder", EMPTY_STRING
+        )
         self.file_path = path
         self.ui.pathLabel.setText(path)
-        # self.meta.retreive_metadata(path, "png")  # temp fixed entension
 
     def display_filename_page(self):
         """Displays filename edit page"""
@@ -78,44 +121,43 @@ class MainWindow(QMainWindow):
         self.ui.stackedWidget.setCurrentIndex(AI_CAPTION_PAGE_INDEX)
         self.current_window_index = AI_CAPTION_PAGE_INDEX
 
-    def write_metadata_to_files(self):
-        process_keywords = self.ui.keywordEdit.text().replace(",", " ").split(" ")
-        process_keywords = [keyword for keyword in process_keywords if keyword != ""]
-        metadata = {
-            "creator": self.ui.creatorEdit.text(),
-            "writer": self.ui.writerEdit.text(),
-            "title": self.ui.titleEdit.text(),
-            "description": self.ui.descriptionEdit.toPlainText(),
-            "keywords": process_keywords,
-            "headline": self.ui.titleEdit.text(),
-            "city": self.ui.cityEdit.text(),
-            "country": self.ui.countryEdit.text(),
-            "state": self.ui.stateEdit.text(),
-            "copyright": self.ui.copyrightEdit.text(),
-            "rights": self.ui.copyrightEdit.text(),
-        }
-        QMessageBox.information(
-            self,
-            "Notification",
-            self.meta.write_metadata(self.file_path, self.get_file_format(), metadata),
-        )
+    def rename_files(self):
+        """Renames all files in the selected path based on the provided inputs (path, format, date, shot, sequence)."""
+        if self.file_path:
+            if self.ui.shotEdit.text() and self.ui.seqEdit.text():
+                date = self.ui.dateEdit.text()
+                shot = int(self.ui.shotEdit.text())
+                seq = int(self.ui.seqEdit.text())
+                ext = self.ui.fileFormatComboBox.currentText()
+                QMessageBox.information(
+                    self,
+                    "Notification",
+                    self.fr.rename_all_files(self.file_path, ext, date, shot, seq),
+                )
+            else:
+                self._display_empty_shot_seq_warning()
+        else:
+            self._display_empty_path_warning()
 
-    def clear_metadata_fields(self):
-        self.ui.creatorEdit.setText("USAF Band Production")
-        self.ui.writerEdit.setText("")
-        self.ui.descriptionEdit.setPlainText("")
-        self.ui.titleEdit.setText("")
-        self.ui.keywordEdit.setText("USAFBand")
-        self.ui.cityEdit.setText("")
-        self.ui.countryEdit.setText("")
-        self.ui.stateEdit.setText("")
-        self.ui.copyrightEdit.setText("Public Domain")
+    def clear_date(self):
+        """Clears the date input field."""
+        self.ui.dateEdit.setText(EMPTY_STRING)
+
+    def clear_all_filename_data(self):
+        """Clears all filename-related input fields."""
+        if self.ui.dateEdit.text() != EMPTY_STRING:
+            self.ui.dateOverrideCheckBox.toggle()
+            self.clear_date()
+        self.ui.shotEdit.setText(EMPTY_STRING)
+        self.ui.seqEdit.setText(EMPTY_STRING)
+
+    def undo_rename(self):
+        """Undoes the last renaming operation."""
+        QMessageBox.information(self, "Notification", self.fr.undo_rename())
 
     def load_metadata(self):
-        """'XMP:Creator', 'XMP:Writer', 'XMP:Description', 'XMP:Title', 'XMP:Keywords'
-        'XMP:City', 'XMP:Country', 'XMP:Headline', 'XMP:State', 'XMP:Copyright'
-        """
-        metadata = self.meta.retreive_metadata(self.file_path, self.get_file_format())
+        """Loads existing metadata from the files in the selected path into the input fields."""
+        metadata = self.meta.retreive_metadata(self.file_path, self._get_file_format())
         self.ui.creatorEdit.setText(metadata["Creator"])
         self.ui.writerEdit.setText(metadata["Writer"])
         self.ui.descriptionEdit.setPlainText(metadata["Description"])
@@ -126,39 +168,58 @@ class MainWindow(QMainWindow):
         self.ui.stateEdit.setText(metadata["State"])
         self.ui.copyrightEdit.setText(metadata["Copyright"])
 
-    def get_file_format(self):
-        if self.current_window_index == FILENAME_PAGE_INDEX:
-            return self.ui.fileFormatComboBox.currentText()
-        return self.ui.metaFileFormatComboBox.currentText()
+    def clear_metadata_fields(self):
+        """Clears all metadata input fields to their default values."""
+        self.ui.creatorEdit.setText(DEFAULT_CREATOR)
+        self.ui.writerEdit.setText(EMPTY_STRING)
+        self.ui.descriptionEdit.setPlainText(EMPTY_STRING)
+        self.ui.titleEdit.setText(EMPTY_STRING)
+        self.ui.keywordEdit.setText(DEFAULT_KEYWORD)
+        self.ui.cityEdit.setText(EMPTY_STRING)
+        self.ui.countryEdit.setText(EMPTY_STRING)
+        self.ui.stateEdit.setText(EMPTY_STRING)
+        self.ui.copyrightEdit.setText(PUBLIC_DOMAIN_COPYRIGHT)
 
-    def clear_date(self):
-        self.ui.dateEdit.setText("")
+    def write_metadata_to_files(self):
+        """Writes metadata to the files in the selected path based on the input fields."""
+        # standardizes keyword format
+        clean_keywords = self.ui.keywordEdit.text().replace(",", " ").split(" ")
+        clean_keywords = [
+            keyword for keyword in clean_keywords if keyword != EMPTY_STRING
+        ]
+        joined_keywords = ", ".join(clean_keywords)
+        metadata = {
+            "creator": self.ui.creatorEdit.text(),
+            "writer": self.ui.writerEdit.text(),
+            "title": self.ui.titleEdit.text(),
+            "description": self.ui.descriptionEdit.toPlainText(),
+            "keywords": joined_keywords,
+            "headline": self.ui.titleEdit.text(),
+            "city": self.ui.cityEdit.text(),
+            "country": self.ui.countryEdit.text(),
+            "state": self.ui.stateEdit.text(),
+            "copyright": self.ui.copyrightEdit.text(),
+            "rights": self.ui.copyrightEdit.text(),
+        }
+        QMessageBox.information(
+            self,
+            "Notification",
+            self.meta.write_metadata(self.file_path, self._get_file_format(), metadata),
+        )
 
-    def clear_all_filename_data(self):
-        if self.ui.dateEdit.text() != "":
-            self.ui.dateOverrideCheckBox.toggle()
-            self.clear_date()
-        self.ui.shotEdit.setText("")
-        self.ui.seqEdit.setText("")
+    def prompt_ai(self):
+        """
+        Prompts the AI to generate a caption based on the input text and displays
+        the generated caption in the AI output box.
+        """
+        stream = self.ai.get_caption(self.ui.aiInputBoxEdit.toPlainText())
+        text = ""
+        for chunk in stream:
+            text += chunk["message"]["content"]
+            self.ui.aiOutputBox.setPlainText(text)
+            QCoreApplication.processEvents()  # to get word by word stream of text
 
-    def rename_files(self):
-        if self.file_path:
-            date = self.ui.dateEdit.text()
-            shot = int(self.ui.shotEdit.text())
-            seq = int(self.ui.seqEdit.text())
-            ext = self.ui.fileFormatComboBox.currentText()
-            QMessageBox.information(
-                self,
-                "Notification",
-                self.fr.rename_all_files(self.file_path, ext, date, shot, seq),
-            )
-        else:
-            self.display_empty_path_warning()
-
-    def undo_rename(self):
-        QMessageBox.warning(self, "Notification", self.fr.undo_rename())
-
-    def display_empty_path_warning(self):
-        if not self.file_path:
-            message = "Please choose a file path"
-            QMessageBox.warning(self, "Path Error", message)
+    def clear_ai_fields(self):
+        """Clears the AI output and input fields."""
+        self.ui.aiOutputBox.setPlainText(EMPTY_STRING)
+        self.ui.aiInputBoxEdit.setPlainText(EMPTY_STRING)
